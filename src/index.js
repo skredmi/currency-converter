7f84810f649ee245b90030f1418d712e9a5e6f8d
Dmitry Skrebkov <skredmi91@gmail.com>
2023-01-14 16:04:33 +0300

    commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import { URL as url, CURRENCY as curr } from "../utils/constants.js";
+
+const convertCurrency = document.querySelector("#convert");
+const convertedCurrency = document.querySelector("#converted");
+const selectedCurrency = document.querySelector("#from");
+const changedCurrency = document.querySelector("#to");
+const input = document.querySelector("#input");
+const resultInput = document.querySelector("#result");
+const date = document.querySelector(".main__current-date");
+
+const changeBackground = (content, select) => {
+  content.textContent = curr[select.value];
+  content.style.backgroundImage = `url(../image/${select.value}.jpg)`;
+};
+
+selectedCurrency.addEventListener("change", () =>
+  changeBackground(convertCurrency, selectedCurrency)
+);
+
+changedCurrency.addEventListener("change", () =>
+  changeBackground(convertedCurrency, changedCurrency)
+);
+
+const result = await fetch(url)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    return data;
+  });
+
+date.textContent = new Date(result.Date).toLocaleDateString("ru");
+
+function calculateCurrency() {
+  if (!input.value) {
+    return;
+  } else if (convertCurrency.textContent === convertedCurrency.textContent) {
+    resultInput.value = input.value;
+  } else if (selectedCurrency.value === "RUB") {
+    resultInput.value = (
+      Number(input.value) / result.Valute[changedCurrency.value].Value
+    ).toFixed(2);
+  } else if (changedCurrency.value === "RUB") {
+    resultInput.value = (
+      Number(input.value) * result.Valute[selectedCurrency.value].Value
+    ).toFixed(2);
+  } else {
+    resultInput.value = (
+      (Number(input.value) * result.Valute[selectedCurrency.value].Value) /
+      result.Valute[changedCurrency.value].Value
+    ).toFixed(2);
+  }
+}
+
+input.addEventListener("input", calculateCurrency);
+changedCurrency.addEventListener("change", calculateCurrency);
+selectedCurrency.addEventListener("change", calculateCurrency);
---
+++ utils/constants.js
@@ -0,0 +1,8 @@
+export const URL = "https://www.cbr-xml-daily.ru/daily_json.js";
+
+export const CURRENCY = {
+  RUB: "Российский рубль",
+  USD: "Американский доллар",
+  EUR: "Евро",
+  GBP: "Фунт стерлингов Соединенного королевства",
+};
